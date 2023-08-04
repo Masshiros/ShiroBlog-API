71a0c51e56a54d3fd3c36700b614530c346aa82d
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2023-08-05 02:06:10 +0700

    Add User Model

---
+++ models/User/user.model.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now(),
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    accountLevel: {
+      type: String,
+      enum: ["bronze", "silver", "gold"],
+      default: "bronze",
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    notificationPreferences: {
+      email: { type: String, default: true },
+      //..other notifications (sms)
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "prefer not to say", "non-binary"],
+    },
+    profileViewers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    likedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
+    passwordResetToken: {
+      type: String,
+    },
+    passwordResetExpires: {
+      type: Date,
+    },
+    accountVerificationToken: {
+      type: String,
+    },
+    accountVerificationExpires: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
